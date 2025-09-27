fbd049cf0df7fc1996e8bc8815b43d42fb278438
Vram Ghazourian <vramghazourian@MacBook-Pro-6.lan>
2025-09-27 01:08:37 -0700

    Add Payment Page with dynamic campus/mail delivery options

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Features from "./components/Features";
-import Pricing from "./components/Pricing";
-import Impact from "./components/Impact";
-import Footer from "./components/Footer";
+import PaymentPage from "./PaymentPage";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <About />
-      <Features />
-      <Pricing />
-      <Impact />
-      <Footer />
+      <PaymentPage />
     </>
   );
 }
